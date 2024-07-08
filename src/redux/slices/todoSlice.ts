d5bfbec971303a7976a54f3576dde86865412ec4
Rambo <sandstorm0819@gmail.com>
2024-07-08 17:26:23 +0300

    feat: add redux logic. create store, create todoSlice

---
+++ src/redux/slices/todoSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { Todo } from '../../models/Todo'
+import { v4 as uuidv4 } from 'uuid'
+
+const initialState = [] as Todo[]
+
+const todoSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: {
+      reducer: (state, action: PayloadAction<Todo>) => {
+        state.push(action.payload)
+      },
+      prepare: (description) => ({
+        payload: {
+          id: uuidv4(),
+          description,
+          completed: false,
+        } as Todo,
+      }),
+    },
+    removeTodo(state, action: PayloadAction<string>) {
+      const index = state.findIndex((todo) => todo.id === action.payload)
+      state.splice(index, 1)
+    },
+    toggleStatusTodo(state, action: PayloadAction<string>) {
+      const index = state.findIndex((todo) => todo.id === action.payload)
+      state[index].completed = !state[index].completed
+    },
+  },
+})
+
+export const { addTodo, removeTodo, toggleStatusTodo } = todoSlice.actions
+export default todoSlice.reducer
