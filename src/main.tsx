2ebf2d0388425828115375ca3aa24128eb3cddb0
Rambo <sandstorm0819@gmail.com>
2024-07-09 18:40:28 +0300

    feat: add redux-persist to store state in local storage

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { Provider } from 'react-redux'
-import { store } from './redux/store'
+import { persistor, store } from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 import { CssBaseline } from '@mui/material'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <CssBaseline />
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <CssBaseline />
+      <App />
+    </PersistGate>
   </Provider>
 )
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
 import todosReducer from './slices/todoSlice'
 import filterReducer from './slices/filterSlice'
 
-export const store = configureStore({
-  reducer: { todos: todosReducer, filter: filterReducer },
+const rootReducer = combineReducers({
+  todos: todosReducer,
+  filter: filterReducer,
 })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
